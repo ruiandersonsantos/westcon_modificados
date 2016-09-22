3881b01394800566afc4db810ee0db1dd9ddef56
user <user@10.5.128.78>
2016-09-22 15:36:56 -0300

    Alteração da função fol_execute do arquivo geraxls_invoice_scx

--- geraxls_invoice_scx.php
+++ geraxls_invoice_scx.php
@@ -1,95 +1,90 @@
 Local VLL_Return, VLO_Object, VLN_Row,VLC_Invoice,VLC_PO,VLN_TAXA, VLC_LogTXT ,VLC_String, VLC_Invoice, VLC_String, VLC_STRINGB, VLC_STRINGC, VLL_Retorno, VLL_Returnb
 Store "" To VLC_Log
-*-SET STEP ON
+
 Store 1 To VLN_Row
-VLL_Returnb = .F.
+
+&& Instanciando objeto do excell
 VLO_Object = Createobject("excel.application")
 VLO_Object.Workbooks.Open(This.VOC_FileName)
 VLL_Retorno = Vartype(VLO_Object) == "O"
-If VLL_Retorno
-	VLL_Retorno = VGO_Gen.FOL_BeginTransaction("F11")
 
-	If VLL_Retorno
-		Do While .T.
-			VLC_Log = ""
-			VLN_Row = VLN_Row + 1
+&& Criando cursor de resultados
+CREATE CURSOR RESULTADO_TMP (INVOICE C(20), DATA c(10), PO C(20), TITULO C(20), VENCIMENTO c(10),;
+							 MOEDA C(10), BRUTODOLAR N(8,2), ABERTODOLAR N(8,2), TAXA N(8,4), BRUTOREAL N(8,2),;
+							 ABERTOREAL N(8,2), VARIACAO N(8,2), TOTAL_HARDWARE N(8,2), TOTAL_SOFTWARE N(8,2),;
+							 TOTAL_SERVICO N(8,2), IRRF N(8,2), MENSAGENS C(200) )
 
-			VLC_Invoice = Nvl(Strt(VLO_Object.Range("A"+Alltrim(Str(VLN_Row))).Text,'.',''),"")
-			VLC_POInvoice = Strt(VLO_Object.Range("B"+Alltrim(Str(VLN_Row))).Text,'.','')
-			If Empty(VLC_Invoice)
-				Exit
-			Endif
+Do While .T.
+	VLC_Log = ""
+	VLN_Row = VLN_Row + 1
 
-
-
-			TEXT TO VLC_STRING TEXTMERGE NOSHOW
-			SELECT U10_001_C FROM U10 (nolock)
-			where U10_001_C = '<<VLC_Invoice>>'
-			ENDTEXT
-
-
-			TEXT TO VLC_STRINGB TEXTMERGE NOSHOW
-			SELECT U10_001_C, 'Invoice Lançada com Número de PO' as LOG FROM U10 (nolock)
-			where U10_001_C = '<<VLC_POInvoice>>'
-			ENDTEXT
-
-
-			VLL_Returnb = VGO_Gen.fol_sqlexec(VLC_STRINGB,"U10_TT")
-			If VLL_Returnb And Reccount("U10_TT")>0
-				VLC_Log = 'Invoice Lançada com Número de PO'
-			Else
-				VLL_Returnb = VGO_Gen.fol_sqlexec(VLC_String,"U10_TT")
-				If VLL_Returnb And !Reccount("U10_TT")>0
-					VLC_Log = 'Invoice não encontrada no sistema'
-				Endif
-			Endif
-
-			VLC_Proc = "exec [Westcon].[Starsoft].[Reports_InvoicePgto]  '" + VLC_Invoice + "','" + VLC_POInvoice + "'" + ","+ + Strtran(Alltrim(Str(This.von_taxa,12,4)),[,],[.])
-			VGO_Gen.fol_sqlexec(VLC_Proc,"TMP_XLS", .F.)
-
-			If Used("TMP_XLS") And Reccount("TMP_XLS")>0
-				If Used("REL") And Reccount("REL")>0
-					Select *, Evl(VLC_Log,Space(200)) As Log From TMP_XLS Into Cursor rel1
-
-					Select rel
-					Insert Into rel Select * From rel1
-
-					VGO_Gen.fol_closetable("rel1")
-				Else
-					Select *, Evl(VLC_Log,Space(200)) As Log From TMP_XLS Into Cursor rel Readwrite
-				Endif
-				VGO_Gen.fol_closetable("TMP_XLS")
-			Else
-				If Used("rel")
-					Select rel
-					Append Blank
-					Replace u10_001_c With VLC_Invoice,;
-						log With VLC_Log In rel
-				Else
-					Select *, Evl(VLC_Log,Space(200)) As Log From TMP_XLS Into Cursor rel Readwrite
-					Select rel
-					Append Blank
-					Replace u10_001_c With VLC_Invoice,;
-						log With VLC_Log In rel
-				Endif
-			Endif
-		Enddo
+	VLC_Invoice = Nvl(Strt(VLO_Object.Range("A"+Alltrim(Str(VLN_Row))).Text,'.',''),"")
+	VLC_POInvoice = Strt(VLO_Object.Range("B"+Alltrim(Str(VLN_Row))).Text,'.','')
+	If Empty(VLC_Invoice)
+		Exit
 	Endif
-Endif
-If Used("rel")
-	Select rel
+
+	WAIT windows "Processando invoice "+ ALLTRIM(VLC_Invoice) + " ..." nowait
+	VLC_Proc = "exec [Westcon].[Starsoft].[Reports_InvoicePgto]  '" + VLC_Invoice + "','" + VLC_POInvoice + "'" + ","+ + Strtran(Alltrim(Str(This.von_taxa,12,4)),[,],[.])
+	VGO_Gen.fol_sqlexec(VLC_Proc,"TMP_XLS")
+
+	If Used("TMP_XLS") And Reccount("TMP_XLS")>0
+	
+		SELECT TMP_XLS
+		GO TOP
+		
+		&& Verificando se a invoice foi lançada com o numero da PO
+		IF ALLTRIM(TMP_XLS.INVOICE) == ALLTRIM(TMP_XLS.PO)
+			VLC_Log = 'Invoice Lançada com Número de PO.'
+		ENDIF
+		
+		&& Verificando se existe titulo para a invoice
+		IF EMPTY(ALLTRIM(NVL(TMP_XLS.TITULO,"")))
+			&& verificanco se já tem log anterior para concatenar
+			IF !EMPTY(VLC_Log)
+				VLC_Log = VLC_Log + ' / Não existe titulo para invoice.'
+			ELSE
+				VLC_Log = 'Não existe titulo para invoice.'
+			endif
+			
+		ENDIF
+		
+		&& verificando e preenchendo o log no cursor temporario
+		IF !EMPTY(VLC_Log)
+			SELECT TMP_XLS
+			replace mensagens WITH VLC_Log IN TMP_XLS
+		endif
+		
+		SELECT TMP_XLS
+		GO TOP
+		scatter memvar memo
+		
+		SELECT RESULTADO_TMP 
+		append blank
+		gather memvar memo
+		
+		VGO_Gen.fol_closetable("TMP_XLS")
+	ELSE
+	
+		SELECT RESULTADO_TMP 
+		append blank
+		replace ;
+			INVOICE WITH VLC_Invoice ,;
+			PO WITH VLC_POInvoice ,;
+			mensagens WITH "Invoice não encontrada no sistema." ;
+		IN RESULTADO_TMP 
+		
+	Endif
+Enddo
+
+If Used("RESULTADO_TMP")
+	Select RESULTADO_TMP
 	Copy To Thisform.VOC_Dir+"\InvoicePgto" Type Xls
-	VGO_Gen.fol_closetable("rel")
+	VGO_Gen.fol_closetable("RESULTADO_TMP")
 Endif
 VLO_Object.Workbooks.Close
 VLO_Object.Quit
 
-
-If VLL_Retorno
-	VGO_Gen.FOL_CommitTrans("F11")
-Else
-	VGO_Gen.FOL_RollBack("F11")
-Endif
 
 
 If VLL_Retorno
@@ -100,6 +95,4 @@ Endif
 
 VLO_Object=.Null.
 
-VGO_Gen.fol_closetable("U10_TT")
-
-Return VLL_Return And DoDefault() And This.Cancel.Click()
+Return DoDefault() And This.Cancel.Click()
